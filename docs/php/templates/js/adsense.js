aa82b8367993742012aa399affbf11ac2391d776
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-01-05 21:39:30 +0200

    adsense

---
+++ docs/php/templates/js/adsense.js
@@ -0,0 +1,21 @@
+<script type="text/javascript"><!--
+
+google_ad_client = "pub-3285794544749294";
+
+/* eksigator */
+
+google_ad_slot = "4270618098";
+
+google_ad_width = 200;
+
+google_ad_height = 200;
+
+//-->
+
+</script>
+
+<script type="text/javascript"
+
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script>
