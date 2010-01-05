fe485d81053877329e47c3ec2b8854a5e31f2c09
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-01-05 23:20:46 +0200

    contact

--- docs/php/modules/iletisim/class.php
+++ docs/php/modules/iletisim/class.php
@@ -3,7 +3,6 @@ class module_iletisim implements modules{
 
     public function controller() {
         $this->parent->loadModuleLanguage();
-        $this->listStaff();
 
 
         if($_POST['message']) {
@@ -18,68 +17,15 @@ class module_iletisim implements modules{
 
 
     public function sendMail($nameSurname, $email, $message) {
-        $nameSurname = htmlspecialchars ( addslashes( $nameSurname ) );
-        $email = htmlspecialchars ( addslashes( $email ) );
-        $message = htmlspecialchars ( addslashes( $message ) );
+        $this->parent->view->assign("senderNameSurname",  htmlspecialchars ( addslashes( $nameSurname ) ) );
+        $this->parent->view->assign("senderEmail", htmlspecialchars ( addslashes( $email ) ));
+        $this->parent->view->assign("senderMessage", htmlspecialchars ( addslashes( $message ) ));
+
+        
+        $template = $this->parent->view->fetch("../modules/iletisim/mails/contact.html");    
+        $this->parent->sendEmail("eksigator@eksigator.com","Iletisim",$template); 
 
         return true;
-    }
-
-
-    function listStaff() {
-        
-        $staff = array( array("title"=>"contact/yonetimKuruluBaskani",
-                              "name"=>"Fatih USTA",
-                              "tel"=>"(+90) 216 486 22 33 - 128",
-                              "email"=>"fatihusta@mastermovie.com.tr"),
-                        
-                         array("title"=>"contact/genelKoordinator",
-                              "name"=>"Emre GÜNAY",
-                              "tel"=>"(+90) 216 486 22 33 - 120",
-                              "email"=>"emregunay@mastermovie.com.tr"),
-
-                         array("title"=>"contact/teknikDanisman",
-                               "sub_title"=>"contact/teknikDanismanSub",
-                              "name"=>"Erkan UMUT",
-                              "tel"=>"(+90) 216 486 22 33 - 121",
-                              "mobile"=>"(+90) 533 433 8 433",
-                              "email"=>"erkanumut@mastermovie.com.tr"),
-
-
-                         array("title"=>"contact/ceo",
-                              "name"=>"Cihan KARADEMİR",
-                              "tel"=>"(+90) 216 486 22 33 - 122",
-                              "email"=>"cihankarademir@mastermovie.com.tr"),
-
-                         array("title"=>"contact/artDirector",
-                              "name"=>"Ümit GÜLMEZ",
-                              "tel"=>"(+90) 216 486 22 33 - 114",
-                              "email"=>"umitgulmez@mastermovie.com.tr"),
-
-                         array("title"=>"contact/vfx",
-                              "name"=>"Abdurrahman SEÇER",
-                              "tel"=>"(+90) 216 486 22 33 - 115",
-                              "email"=>"abdsecer@mastermovie.com.tr"),
-
-                         array("title"=>"contact/muhasebe",
-                              "name"=>"Yalçın KARADAŞ",
-                              "tel"=>"(+90) 216 486 22 33 - 119",
-                              "email"=>"yalcinkaradas@mastermovie.com.tr"),
-
-                         array("title"=>"contact/koordinator",
-                              "name"=>"Bilal İNCE",
-                              "tel"=>"(+90) 216 486 22 33 - 118",
-                              "email"=>"bilalince@mastermovie.com.tr"),
-
-                         array("title"=>"contact/musteriIliskileri",
-                              "name"=>"Rabia YÜCEYURT",
-                              "tel"=>"(+90) 216 486 22 33 - 111",
-                              "email"=>"rabiayuceyurt@mastermovie.com.tr")
-                            
-                    );
-
-
-            $this->parent->view->assign("staff", $staff);
     }
 
 
