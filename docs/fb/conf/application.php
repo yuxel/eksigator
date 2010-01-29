7d78f4863657e1a961cec807a1515176b888a81e
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-01-30 01:39:57 +0200

    Config file

--- docs/fb/conf/application.php
+++ docs/fb/conf/application.php
@@ -3,6 +3,6 @@
 require_once 'facebookClient/facebook.php';
 require_once "../php/3rdParty/smarty/libs/Smarty.class.php";
 
-$appapikey = '62fff396d245acbc100fb0f4d1e37e60';
-$appsecret = '65af76bcd01839ca44ff73cf255ee0aa';
+$appapikey = 'HERE_COMES_API_KEY';
+$appsecret = 'HERE_COMES_SECRET';
 
