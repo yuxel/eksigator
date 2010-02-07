ecba6c600277b456b2ace0328284e98d7450cf0e
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-02-07 17:16:35 +0200

    template main

--- docs/fb/lib/Eksigator.php
+++ docs/fb/lib/Eksigator.php
@@ -23,7 +23,7 @@ class Eksigator
    
         $this->facebookUser = $this->facebook->require_login();
 
-        if(!$this->getUserData($this->facebookUser)){
+        if($this->getUserData($this->facebookUser)){
             $module = "Register";
         } 
         else{
