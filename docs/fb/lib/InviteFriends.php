ee0f07593d2ec232b2a8c38d0db7ee7db4773336
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-02-23 23:48:53 +0200

    invite

--- docs/fb/lib/InviteFriends.php
+++ docs/fb/lib/InviteFriends.php
@@ -4,18 +4,6 @@ class InviteFriends extends ModuleBase
 {
 
     function run(){
-
-       $content = "<fb:req-choice url=\"http://apps.facebook.com/eksigator\" label=\"Put this on your profile\"/>"; 
-
-        $content = htmlspecialchars($content);       
-        $content = htmlspecialchars($content);       
-
-       
-
-        var_dump( $content ); 
-
-        $this->view->assign("reqContent", $content); 
-    
         $this->rightContent = $this->view->fetch ("invite.html");
     }
 
