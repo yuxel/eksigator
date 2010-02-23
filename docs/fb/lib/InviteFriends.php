8735d4dc25e61b44be16b7ca99e3539267e65d15
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-02-23 23:34:37 +0200

    invite

--- docs/fb/lib/InviteFriends.php
+++ docs/fb/lib/InviteFriends.php
@@ -5,12 +5,13 @@ class InviteFriends extends ModuleBase
 
     function run(){
 
-       $content = "<fb:name uid=\"".$user."\" firstnameonly=\"true\" shownetwork=\"false\"/> has started using <a href=\"http://apps.facebook.com/".$app_url."/\">".$app_name."</a> and thought it's so cool even you should try it out!\n".
-        "<fb:req-choice url=\"".$this->parent->fbUrl."\" label=\"Put ".$app_name." on your profile\"/>"; 
+       $content = "<fb:req-choice url=\"http://apps.facebook.com/eksigator\" label=\"Put this on your profile\"/>"; 
 
 
        $content = htmlentities($content,ENT_COMPAT,'UTF-8');
         
+
+       var_dump ( $content );
 
         $this->view->assign("reqContent", $content); 
     
