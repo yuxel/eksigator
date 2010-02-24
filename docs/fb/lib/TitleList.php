53a6c7cf2635973e189aa017725529a0f701f12d
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-02-24 13:01:29 +0200

    List

--- docs/fb/lib/TitleList.php
+++ docs/fb/lib/TitleList.php
@@ -4,7 +4,20 @@ class TitleList extends ModuleBase
 {
 
     function run(){
+        $titles = $this->getTitles();
+
+        $this->view->assign("titles", $titles);
         $this->rightContent = $this->view->fetch ("list.html");
+    }
+
+    function getTitles() {
+
+        $email = $this->parent->userData['email'];
+        $apiKey = $this->parent->userData['apiKey'];
+
+        $datas = $this->parent->getData($email, $apiKey);
+
+        return $datas;
     }
 
 
