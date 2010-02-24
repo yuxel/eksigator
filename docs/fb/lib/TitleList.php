f7639c1ab1b652750665614547874e9c6e6ca53e
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-02-24 13:07:50 +0200

    Add to list

--- docs/fb/lib/TitleList.php
+++ docs/fb/lib/TitleList.php
@@ -4,10 +4,20 @@ class TitleList extends ModuleBase
 {
 
     function run(){
+
+        if($_POST['newTitle']) {
+            $title = $_POST['newTitle'];
+            $this->addToList($title);
+        }
+         
+
         $titles = $this->getTitles();
 
         $this->view->assign("titles", $titles);
         $this->rightContent = $this->view->fetch ("list.html");
+
+
+
     }
 
     function getTitles() {
@@ -18,6 +28,25 @@ class TitleList extends ModuleBase
         $datas = $this->parent->getData($email, $apiKey);
 
         return $datas;
+    }
+
+
+    function addTitle($title) {
+        $email = $this->parent->userData['email'];
+        $apiKey = $this->parent->userData['apiKey'];
+
+        $addUrl = $this->apiUrl.$email."/".$apiKey."/addToList/".$title;
+        file_get_contents($addUrl);
+
+    }
+
+    function removeTitle($title) {
+        $email = $this->parent->userData['email'];
+        $apiKey = $this->parent->userData['apiKey'];
+
+        $addUrl = $this->apiUrl.$email."/".$apiKey."/removeFromList/".$title;
+        file_get_contents($addUrl);
+
     }
 
 
