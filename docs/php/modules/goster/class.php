2ac4ada9678053d834c9d44462e4acf319a24466
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-03-31 18:38:13 +0300

    show changes

--- docs/php/modules/goster/class.php
+++ docs/php/modules/goster/class.php
@@ -2,7 +2,96 @@
 class module_goster implements modules{
 
     public function controller() {
-        return $this->parent->getModuleTemplate("show");
+
+        $this->userEmail = $_SESSION['eksigator']['email'];
+        $this->userApiKey = $_SESSION['eksigator']['apiKey'];
+        $this->apiUrl = "http://api.eksigator.com/";
+        $this->requestUrl = $this->apiUrl.$this->userEmail."/".$this->userApiKey."/";
+
+
+        if($_SESSION['eksigator']) {
+
+            $isAjax = $_GET['ajax'];
+
+
+            $page =  $this->parent->url->urlStrings;
+            
+            $action = $page->param_1;
+            $title  = $_POST['title'];
+            $title  = urlencode($title);
+
+            switch($action) {
+                case "addToList":
+                    $data = $this->addToUsersList($title);
+                    break;
+
+                case "removeFromList":
+                    $data = $this->removeFromUsersList($title);
+                    break;
+
+                case "setItemAsRead":
+                    $data = $this->setItemAsRead($title);
+                    break;
+
+               case "getList":
+               default:
+                    $data = $this->getUsersList();
+                    break;
+            }
+
+            if($isAjax) {
+                echo $data;
+                exit;
+            }
+            else{
+                $data = json_decode($data);
+                $this->parent->view->assign("usersList", $data);
+            }
+
+            
+            return $this->parent->getModuleTemplate("show");
+        }
     }
+
+
+    public function getUsersList() {
+        $command = $this->requestUrl. "getList";
+        $json = file_get_contents($command);
+
+        $array = json_decode($json);
+
+        foreach($array as $item) {
+            $newArray[$item->status] [] = $item;
+        }
+
+        $json = json_encode($newArray);
+
+        return $json;
+    }
+
+    public function addToUsersList($title) {
+        var_dump($title);
+        $command = $this->requestUrl . "addToList/".$title;
+        $json = file_get_contents($command);
+
+        return $json;
+    }
+
+    public function removeFromUsersList($title) {
+        $command = $this->requestUrl . "removeFromList/".$title;
+        $json = file_get_contents($command);
+
+        return $json;
+    }
+
+    public function setItemAsRead($title) {
+        $command = $this->requestUrl . "setItemAsRead/".$title;
+        $json = file_get_contents($command);
+
+        return $json;
+
+    }
+
+
 
 }
--- docs/php/modules/goster/templates/js/script.js
+++ docs/php/modules/goster/templates/js/script.js
@@ -1,86 +1,158 @@
+var inputText="Takip etmek istediğiniz başlığı girin";
+var siteUrl = "http://" + window.location.hostname + "/goster/";
+var currentUrl = window.location.href;
+var sozlukUrl = "http://sozluk.sourtimes.org/";
+var maxID = 299999999999;
+var newTitleCount=0;
+
 $(document).ready( function() {
-   
-      $("#checkEmail").change( checkEmail ); 
-      $("#checkPassword, #checkPasswordAgain").change( checkPassword ); 
+    $("#newTitle").val(inputText);
 
-      $("#submitButton").click( submitForm );
+    $("#newTitle").focus( checkInputFieldIn );
+    $("#newTitle").blur( checkInputFieldOut );
 
+    $("#newTitleButton").click( addTitleToList );
+
+    $(".toggle").click( toggleNotUpdatedFields );
+
+    $(".setReadAndGoToTitle").click( setReadAndGoToTitle );
+    $(".goToTitle").click( goToTitle );
+
+    $(".remove").click ( removeTitleFromList );
 });
 
+function toggleNotUpdatedFields() {
+    currentClasses = $(this).attr("class");
 
-function checkPassword() {
+    if( currentClasses.indexOf("toggleUp") > -1 ) {
+        $(this).removeClass("toggleUp");
+        $("#hiddenTitles").hide("fast");
+    }
+    else{
+        $(this).addClass("toggleUp");
+        $("#hiddenTitles").show("fast");
+    }
+}
 
-        value = $("#checkPassword").val();
 
-        if( value != undefined) {
+function checkInputFieldIn() {
+    currentText = $(this).val();
+    if( currentText == inputText ) {
+        $(this).removeClass("wait");
+        $(this).val('');
+    }
+}
 
-            if(value.length > 5) {
+function checkInputFieldOut() {
+    currentText = $(this).val();
+    if( currentText.length == 0 ) {
+        $(this).addClass("wait");
+        $(this).val(inputText);
+    }
+}
 
-                again = $("#checkPasswordAgain").val();
-                $(".passwordError").html ( '' );
 
-                if( again != value ) {
-                    $(".passwordAgainError").html ( passwordAgainError );
-                    return false;
-                }
-                else{
-                    $(".passwordAgainError").html ( '' );
-                    return true;
-                }
+function addTitleToList() {
+    title = $("#newTitle").val();
+    command = siteUrl + "addToList/?ajax=1";
 
-                
-            }
-            else {
-                $(".passwordError").html ( passwordError );
-                return false;
-            }
+    $.post(command, {"title": title}, function(data) {
+
+        if( isTitleExists(title) ) {
+            alert("Bu başlığı zaten takip ediyorsunuz");
+        }
+        else{
+            
+            mod = "line"+ (newTitleCount%2);
+
+            newTitleCount++;
+            newId = "new"+newTitleCount+"_"+maxID;
+            newDiv =  '<div class="row '+mod+'"> &nbsp; <span class="remove">x</span> &nbsp; <a class="goToTitle"  name="'+newId+'" id="'+newId+'">'+title+'</a>   </div>';
+            $("#hiddenTitles").append(newDiv);
+
+            $("#newTitle").val('');
+            alert("'"+title+"' başlığı takip listenize eklendi");
+            $("#newTitle").blur();
+
 
         }
 
-        return true;
+        $(".remove").click ( removeTitleFromList );
+    });
+}
 
+function setReadAndGoToTitle() {
+    split = $(this).attr("id").split("_");
+
+    title = $(this).html();
+    lastId = split[1];
+
+    urlToGo = sozlukUrl + "show.asp?t="+title+"&i="+lastId;
+    command = siteUrl + "setItemAsRead/?ajax=1";
+    $.post(command, {"title": title}, function(data) {
+        window.open (urlToGo );
+        window.location.href = siteUrl;
+    });
 
 }
 
+function goToTitle() {
+    split = $(this).attr("id").split("_");
 
-function checkEmail() {
-    value = $("#checkEmail").val();
+    title = $(this).html();
+    lastId = split[1];
 
-    if(value.length > 0 && validateEmail(value)) {
-
-        $(".emailError").html ( '' );
-        return true;
-    }
-    else {
-        $(".emailError").html ( emailError );
-        return false;
-    }
-
+    urlToGo = sozlukUrl + "show.asp?t="+title+"&i="+lastId;
+    window.open (urlToGo );
 }
 
 
-function submitForm() {
+function removeTitleFromList() {
+    parentDiv = $(this).parent("div");
+    split = $("a", parentDiv).attr("id").split("_");
 
-    emailStatus = checkEmail();
+    title = $("a", parentDiv).html();
+    lastId = split[1];
 
-    passwordStatus = checkPassword();
+    var answer = confirm("'"+title+"' başlığını takip listenizden çıkarmak istediğinize emin misiniz?")
+    if (answer){
 
+        command = siteUrl + "removeFromList/?ajax=1";
+        $.post(command, {"title": title}, function(data) {
+            $(parentDiv).hide();
+            reOrderList();
 
-    if(emailStatus && passwordStatus ) {
-        //burda form gonderilir
-    }
-    else{
-        $("#submitButton").addClass("formError"); 
-        return false;
+        });
     }
 }
 
 
+function reOrderList( ) {
+    $(".row").removeClass("line0");
+    $(".row").removeClass("line1");
 
-function validateEmail(address) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(address)){
-        return (true);
-    }
-    return (false);
+    currentId = 1;
+    $(".row:visible").each( function() {
+        if(currentId == 1) {
+            currentId = 0;
+        }
+        else {
+            currentId = 1;
+        }
+        $(this).addClass("line"+currentId);
+    });
+}
 
+
+function isTitleExists(title) {
+    
+    $(".row a").each ( function() {
+        aTitle = $(this).html();
+
+        if( title == aTitle ) {
+            return true;
+        }
+    });
+
+    return false;
 }
